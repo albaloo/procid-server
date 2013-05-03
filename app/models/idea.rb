45611e0011c000d7dc1046e74386dde33576ccfd
znicholson92 <znichol2@illinois.edu>
2013-05-02 19:07:20 -0500

    added destroy_idea function

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -23,4 +23,25 @@ class Idea
     result = result.sort {|x,y| x.commented_at <=> y.commented_at}
     return result
   end
+  
+  def destroy_idea
+  	if(destroy)									#return true if idea has already been destroyed
+  		return true
+  	end
+  	Tag.first(:comment => comment).destroy		#destroy associated tag	
+  	criteria_statuses.comment.destroy			#destroy associated criteria statuses and their comments
+  	criteria_statuses.comment
+  	criteria_statuses.destroy
+  	com=comment									#remove association to comment the idea was created in
+  	com.ideasource=nil
+  	com.save
+  	comment=nil
+  	comments.each do |t|						#remove association to references
+  		t.idea=nil
+  		t.save
+  	end
+  	comments.clear
+  	return destroy								#call destroy for this idea and return result
+  end
+  
 end
